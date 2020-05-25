851ce58e76fc3a0c653cf9100a3cd26fc87567ef
Asif <asif10@gmail.com>
2020-05-25 21:30:30 +0100

    initial commit of files

---
+++ src/test/java/uk/co/lightapps/app/earnings/EarningsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.lightapps.app.earnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
