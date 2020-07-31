d392f97501613b9b6e6d349dc15938cb86ef1034
Asif <asif10@gmail.com>
2020-07-31 17:00:09 +0100

    Updated

--- src/main/java/uk/co/lightapps/app/earnings/SocketsController.java
+++ src/main/java/uk/co/lightapps/app/earnings/SocketsController.java
@@ -3,6 +3,7 @@ package uk.co.lightapps.app.earnings;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -21,7 +22,7 @@ public class SocketsController {
     @Autowired
     private SimpMessagingTemplate template;
 
-    @MessageMapping("/send")
+    @MessageMapping("/open/send")
     @SendTo("/topic/greeting")
     public String topic() {
         return new Gson().toJson(message());
@@ -35,5 +36,17 @@ public class SocketsController {
     public void publishUpdates() {
         System.out.println("Message: " + message());
         template.convertAndSend("/topic/greeting", message());
+    }
+
+    @PostMapping("/open/error")
+    public void publishError() {
+        System.out.println("Message: " + message());
+        template.convertAndSend("/topic/fake", message());
+    }
+
+    @MessageExceptionHandler
+    public String handleException(Throwable exception) {
+        template.convertAndSend("/errors", exception.getLocalizedMessage());
+        return exception.getMessage();
     }
 }
