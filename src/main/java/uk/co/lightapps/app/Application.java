e78229cde40dbb9dc35ae40bf0fb4897aff66fda
Asif <asif10@gmail.com>
2020-11-02 21:33:36 +0000

    done

--- src/main/java/uk/co/lightapps/app/Application.java
+++ src/main/java/uk/co/lightapps/app/Application.java
@@ -1,13 +1,13 @@
-package uk.co.lightapps.app.earnings;
+package uk.co.lightapps.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EarningsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EarningsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
